3e22db570ff6674bf38c9de1391ee60082d782f9
leskoarpi <71691273+leskoarpi@users.noreply.github.com>
2022-01-10 17:10:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
+import math
 from math import sqrt
+
 def tavolsag_n_d(*nums):
-    lista=[]
-    result = 0
-    index = 0
-    for i in nums:
-        lista.append(nums[index])
-        index+=1
-    index = 0
-    finalresult = 0
-    retkeslista =[]
-    for x in lista:
-        negyzet = lista[index]**2
-        retkeslista.append(negyzet)
-        index += 1
-    finalresult = sqrt(sum(retkeslista))
-
-    return finalresult
-
-
+    return math.sqrt( sum( [ num ** 2 for num in nums] ))
+    
 print(tavolsag_n_d(1,2,3,5,6,14))
